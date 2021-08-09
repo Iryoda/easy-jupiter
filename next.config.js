5ef41799857e7ec51926e6e4fd13fb8251c96c4d
Iryoda <iryodap@gmail.com>
2021-08-09 10:45:22 -0300

    :lock: feat: google secrets

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+    publicRuntimeConfig: {
+        apiKey: `${process.env.API_KEY}`,
+        clientId: `${process.env.CLIENT_ID}`,
+        discoveryDocs: [`${process.env.DISCOVERY_DOCS}`],
+        scope: `${process.env.SCOPE}`
+    },
     future: {
         webpack5: true
     }
