8fbc628b4c49e0d678ac022f3ac262c637f6bffd
Mateus Roni <mateus-roni@hotmail.com>
2021-08-13 19:24:24 -0300

    fix: centralized content (⌐■_■)

--- src/components/GlobalContainer/styles.ts
+++ src/components/GlobalContainer/styles.ts
@@ -2,5 +2,6 @@ import styled from 'styled-components'
 
 export const GlobalContainer = styled.main`
     max-width: 100vw;
+    height: 100vh;
     background: ${(props) => props.theme.colors.background};
 `
--- src/templates/Base/styles.ts
+++ src/templates/Base/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Content = styled.div`
-    height: 100%;
+    height: calc(100vh - 9.6rem);
     display: flex;
     flex-direction: column;
     align-items: center;
