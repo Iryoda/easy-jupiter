1bfd71d3e49c99c73ede737d1511d51786e0a3a9
Augusto Iryoda <iryodap@gmail.com>
2022-01-12 23:43:51 -0300

    :sparkles: feat: ui-improviments

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,15 +4,19 @@ import media from 'styled-media-query';
 export const Wrapper = styled.div`
     width: 100%;
     height: 9.6rem;
-    /* background: ${(props) => props.theme.colors.purple}; */
+
     display: flex;
     align-items: center;
     justify-content: space-between;
+
     padding: 0rem 4.2rem;
+
     h1 {
         font-size: 2.4rem;
         color: #fff;
         font-weight: 400;
+
+        cursor: pointer;
     }
 
     ${media.greaterThan('medium')`
